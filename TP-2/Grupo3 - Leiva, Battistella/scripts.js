53f4cdf49236bbb3f390cf236a2629f130a474b7
Tomas Battistella <tomas.battistella0@gmail.com>
2025-07-09 22:52:58 -0300

    Se agrego la funcionalidad de cargar las series en Strapi

--- TP-2/Grupo3 - Leiva, Battistella/scripts.js
+++ TP-2/Grupo3 - Leiva, Battistella/scripts.js
@@ -1,3 +1,9 @@
+const frases = [
+  "La vida es un viaje, no un destino.",
+  "El conocimiento es poder.",
+  "La creatividad es la inteligencia divirtiéndose.",
+];
+
 function mostrarFrase(frases) {
   const divPrin = document.querySelector(".contenido_principal");
   divPrin.classList.add("oculto");
@@ -58,11 +64,45 @@ async function setMovies() {
   }
 }
 
-const frases = [
-  "La vida es un viaje, no un destino.",
-  "El conocimiento es poder.",
-  "La creatividad es la inteligencia divirtiéndose.",
-];
+const STRAPI_TOKEN = '099da4cc6cbb36bf7af8de6f1f241f8c81e49fce15709c4cfcae1313090fa2c1ac8703b0179863b4eb2739ea65ae435e90999adb870d49f9f94dcadd88999763119edca01a6b34c25be92a80ed30db1bcacb20df40e4e7f45542bd501f059201ad578c18a11e4f5cd592cb25d6c31a054409caa99f11b6d2391440e9c72611ea';
+async function guardarSerieEnStrapi(serie) {
+  await fetch('https://gestionweb.frlp.utn.edu.ar/api/g3-series', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${STRAPI_TOKEN}`
+    },
+    body: JSON.stringify({
+      data: {
+        titulo: serie.nombre,
+        sinopsis: serie.sinopsis,
+        genero: serie.generos[0] || "desconocido",
+        cant_votos: serie.cantidadDeVotos,
+        prom_votos: serie.promedioDeVotos
+      }
+    })
+  });
+  
+  if (!res.ok) {
+    const msg = await res.text();
+    console.error('Error al guardar serie:', res.status, msg);
+    throw new Error(`Falló con status ${res.status}`);
+  }
+}
+
+async function subirSeriesStrapi() {
+  try {
+    const series = await setMovies(); 
+
+    for (const serie of series) {
+      await guardarSerieEnStrapi(serie); 
+    }
+
+    console.log('Series cargadas correctamente en Strapi');
+  } catch (error) {
+    console.error('Error al cargar series:', error);
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const cargarDatosLink = document.querySelector('.setDatos');
@@ -71,6 +111,5 @@ document.addEventListener('DOMContentLoaded', () => {
       setMovies();
   });
 });
-
 
 mostrarFrase(frases);
