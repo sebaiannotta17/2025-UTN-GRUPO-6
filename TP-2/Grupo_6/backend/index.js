ec29108b97def0c4f107c776dddd16c5e5d9fbe6
JulianColoma <juliancoloma751@gmail.com>
2025-07-16 17:25:59 -0300

    Entrega final con grafico de nacionalidades

--- TP-2/Grupo_6/backend/index.js
+++ TP-2/Grupo_6/backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { cargarPelicula, recuperarPeliculas } from "./services/strapi.js";
+import { cargarPelicula, eliminarTodo, recuperarPeliculas } from "./services/strapi.js";
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -17,6 +17,7 @@ const port = 3000;
 const app = express()
 
 app.use(cors(corsOptions))
+app.use(express.json());
 
 
 app.get("/", (req, res) => {
@@ -42,6 +43,14 @@ app.get("/recuperar-matrix", async (req, res) => {
     res.status(500).json({ error: "Algo falló", detalle: err.message });
   }
 });
+app.delete("/eliminar-todo", async (req, res)=>{
+  try {
+    await eliminarTodo();
+    res.status(200).json({ mensaje: "Peliculas eliminadas"});
+  } catch (err) {
+    res.status(500).json({ error: "Algo falló", detalle: err.message });
+  }
+})
 
 app.listen(port,'0.0.0.0', () => {
   console.log(`Servidor backend escuchando en puerto ${port}`);
